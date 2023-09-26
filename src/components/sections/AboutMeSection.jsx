5cf666a0f62f456f568c993f0df3e42e9c969162
Matias Gaete <m-gaete@doterra.com>
2023-09-26 01:13:57 -0300

    removed dark mode and changed all style

--- src/components/sections/AboutMeSection.jsx
+++ src/components/sections/AboutMeSection.jsx
@@ -1,11 +1,14 @@
 const AboutMeSection = () => {
 	return (
-		<div className='container info'>
-			<span>
-				Soy Ingeniero en Software 💻 que trabaja actualmente para plataformas de
-				e-commerce 🛒, en la empresa se utilizan los framework de Angular para
-				Frontend y Spring en Backend 👨🏻‍💻
-			</span>
+		<div className='about-me-section'>
+			<h2>Acerca de mi</h2>
+			<div className='container info'>
+				<span>
+					Soy Ingeniero en Software 💻 que trabaja actualmente para plataformas
+					de e-commerce 🛒, en la empresa se utilizan los framework de Angular
+					para Frontend y Spring en Backend 👨🏻‍💻
+				</span>
+			</div>
 		</div>
 	)
 }
--- src/components/sections/TechnologiesSection.jsx
+++ src/components/sections/TechnologiesSection.jsx
@@ -10,34 +10,44 @@ import logoHtml from '../../assets/html-5-logo.svg'
 import logoCss from '../../assets/css-logo.svg'
 import logoScss from '../../assets/scss-logo.svg'
 import logoJava from '../../assets/java-logo.svg'
-import logoPHP from '../../assets/php-logo.svg'
+
+import logoVS from '../../assets/vscode-logo.svg'
+import logoSourceTree from '../../assets/sourcetree-logo.svg'
+import logoGithub from '../../assets/github-logo.svg'
+import logoIntellij from '../../assets/intellij-logo.svg'
+import logoChatGPT from '../../assets/chatGPT-logo.svg'
+import logoNotion from '../../assets/notion-logo.svg'
 
 const Technologies = React.lazy(() => import('../Technologies'))
 
 const TechnologiesSection = () => {
-	const frontEnd = [
-		['Angular', logoAngular],
-		['React', logoReact],
-		['TypeScript', logoTs],
-		['JavaScript', logoJs],
-		['HTML', logoHtml],
-		['CSS', logoCss],
-		['SCSS', logoScss]
+	const frameworks = [
+		{ title: 'Angular', icon: logoAngular },
+		{ title: 'React', icon: logoReact },
+		{ title: 'TypeScript', icon: logoTs },
+		{ title: 'JavaScript', icon: logoJs },
+		{ title: 'HTML', icon: logoHtml },
+		{ title: 'CSS', icon: logoCss },
+		{ title: 'SCSS', icon: logoScss },
+		{ title: 'Node JS', icon: logoNode },
+		{ title: 'Java', icon: logoJava }
 	]
-	const backEnd = [
-		['Node JS', logoNode],
-		['Java', logoJava],
-		['PHP', logoPHP]
+	const tools = [
+		{ title: 'VSCode', icon: logoVS },
+		{ title: 'Intellij', icon: logoIntellij },
+		{ title: 'ChatGPT', icon: logoChatGPT },
+		{ title: 'SourceTree', icon: logoSourceTree },
+		{ title: 'Github', icon: logoGithub },
+		{ title: 'Notions', icon: logoNotion }
 	]
 
 	return (
-		<LazyLoad>
+		<LazyLoad className='technologies-section'>
+			<h2>Tecnologías</h2>
 			<span>He utilizado las siguientes tecnologias:</span>
-			<div
-				className='container technologies'
-			>
-				<Technologies title='FrontEnd' icons={frontEnd} />
-				<Technologies title='BackEnd' icons={backEnd} />
+			<div className='container technologies'>
+				<Technologies title='Tecnologías' icons={frameworks} />
+				<Technologies title='Herramientas' icons={tools} />
 			</div>
 		</LazyLoad>
 	)
